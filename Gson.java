73f0653e0a118c487a3e143d47b52752d8914033
JavierLutin03 <143854152+JavierLutin03@users.noreply.github.com>
2024-10-03 19:22:57 -0600

    Create Gson.java

---
+++ Gson.java
@@ -0,0 +1,22 @@
+package Protecto2;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.lang.reflect.Type;
+import java.util.List;
+
+
+
+public class Gson {
+
+	public List<Producto> fromJson(FileReader reader, Type productoListType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Producto> fromJson1(List<Producto> productos, FileWriter writer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
